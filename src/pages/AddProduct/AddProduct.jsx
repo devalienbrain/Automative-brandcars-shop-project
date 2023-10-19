a3a75b526fbdf0e1e17041a8d4a9d749d5e76ce2
Alien Brain <murad.dub@gmail.com>
2023-10-19 16:26:11 +0600

    add product send to server done

--- src/pages/AddProduct/AddProduct.jsx
+++ src/pages/AddProduct/AddProduct.jsx
@@ -1,42 +1,39 @@
-// import Swal from 'sweetalert2'
+// import Swal from "sweetalert2";
 
 const AddProduct = () => {
-  const handleAddCoffee = (event) => {
+  const handleAddAProduct = (event) => {
     event.preventDefault();
 
     const form = event.target;
 
+    const image = form.image.value;
     const name = form.name.value;
-    const quantity = form.quantity.value;
-    const supplier = form.supplier.value;
-    const taste = form.taste.value;
-    const category = form.category.value;
-    const details = form.details.value;
-    const photo = form.photo.value;
+    const brand = form.brand.value;
+    const type = form.type.value;
+    const price = form.price.value;
+    const description = form.description.value;
+    const ratings = form.ratings.value;
 
-    const newCoffee = {
+    const newProduct = {
+      image,
       name,
-      quantity,
-      supplier,
-      taste,
-      category,
-      details,
-      photo,
+      brand,
+      type,
+      price,
+      description,
+      ratings,
     };
 
-    console.log(newCoffee);
+    console.log(newProduct);
 
     // send data to the server
-    fetch(
-      "https://coffee-store-server-74xiae2di-jhankarphero.vercel.app/coffee",
-      {
-        method: "POST",
-        headers: {
-          "content-type": "application/json",
-        },
-        body: JSON.stringify(newCoffee),
-      }
-    )
+    fetch("http://localhost:3000/products", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newProduct),
+    })
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
@@ -52,118 +49,121 @@ const AddProduct = () => {
   };
 
   return (
-    <div className="bg-[#F4F3F0] p-24">
-      <h2 className="text-3xl text-red-800 text-center font-extrabold">
-        Add a Product
+    <div className="p-5 w-full lg:w-1/2 mx-auto">
+      <h2 className="text-3xl text-red-800 text-center font-black">
+        ADD A PRODUCT
       </h2>
-      <form onSubmit={handleAddCoffee}>
+      <form onSubmit={handleAddAProduct}>
         {/* form */}
         <div className="mb-8">
           {/* 1_image field */}
           <div className="form-control w-full">
             <label className="label">
-              <span className="label-text">Photo URL</span>
+              <span className="label-text text-lg font-bold">Image</span>
             </label>
             <label className="input-group">
               <input
                 type="text"
-                name="photo"
-                placeholder="Photo URL"
-                className="input input-bordered w-full"
+                name="image"
+                placeholder="product image url"
+                className="input input-bordered rounded-xl drop-shadow-lg w-full"
               />
             </label>
           </div>
-          {/* 2 */}
+          {/* 2_name field */}
           <div className="form-control w-full">
             <label className="label">
-              <span className="label-text">Category</span>
-            </label>
-            <label className="input-group">
-              <input
-                type="text"
-                name="category"
-                placeholder="Category"
-                className="input input-bordered w-full"
-              />
-            </label>
-          </div>
-          {/* 3 */}
-          <div className="form-control w-full">
-            <label className="label">
-              <span className="label-text">Taste</span>
-            </label>
-            <label className="input-group">
-              <input
-                type="text"
-                name="taste"
-                placeholder="Taste"
-                className="input input-bordered w-full"
-              />
-            </label>
-          </div>
-          {/* 4 */}
-          <div className="form-control w-full">
-            <label className="label">
-              <span className="label-text">Available Quantity</span>
-            </label>
-            <label className="input-group">
-              <input
-                type="text"
-                name="quantity"
-                placeholder="Available Quantity"
-                className="input input-bordered w-full"
-              />
-            </label>
-          </div>
-          {/* 5 */}
-          <div className="form-control w-full">
-            <label className="label">
-              <span className="label-text">Supplier Name</span>
-            </label>
-            <label className="input-group">
-              <input
-                type="text"
-                name="supplier"
-                placeholder="Supplier Name"
-                className="input input-bordered w-full"
-              />
-            </label>
-          </div>
-          {/* 6 */}
-          <div className="form-control w-full">
-            <label className="label">
-              <span className="label-text">Coffee Name</span>
+              <span className="label-text text-lg font-bold">Name</span>
             </label>
             <label className="input-group">
               <input
                 type="text"
                 name="name"
-                placeholder="Coffee Name"
-                className="input input-bordered w-full"
+                placeholder="product name"
+                className="input input-bordered rounded-xl drop-shadow-lg w-full"
               />
             </label>
           </div>
-          {/* 7 */}
+          {/* 3_brand name field */}
           <div className="form-control w-full">
             <label className="label">
-              <span className="label-text">Details</span>
+              <span className="label-text text-lg font-bold">Brand Name</span>
             </label>
             <label className="input-group">
               <input
                 type="text"
-                name="details"
-                placeholder="Details"
-                className="input input-bordered w-full"
+                name="brand"
+                placeholder="brand name"
+                className="input input-bordered rounded-xl drop-shadow-lg w-full"
               />
             </label>
           </div>
-        </div>
-        <div className="text-center">
-          <input
-            type="submit"
-            value="Add Product"
-            className="px-8 py-3 bg-red-50 text-red-800 border border-red-900 hover:bg-red-100 rounded-xl drop-shadow-2xl font-bold text-xl"
-          />
+          {/* 4_type field */}
+          <div className="form-control w-full">
+            <label className="label">
+              <span className="label-text text-lg font-bold">Type</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                name="type"
+                placeholder="type of the product"
+                className="input input-bordered rounded-xl drop-shadow-lg w-full"
+              />
+            </label>
+          </div>
+          {/* 5_price field */}
+          <div className="form-control w-full">
+            <label className="label">
+              <span className="label-text text-lg font-bold">Price</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="number"
+                name="price"
+                placeholder="price of the product"
+                className="input input-bordered rounded-xl drop-shadow-lg w-full"
+              />
+            </label>
+          </div>
+          {/* 6_short description field */}
+          <div className="form-control w-full">
+            <label className="label">
+              <span className="label-text text-lg font-bold">
+                Short Description
+              </span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                name="description"
+                placeholder="short description of the product"
+                className="input input-bordered rounded-xl drop-shadow-lg w-full"
+              />
+            </label>
+          </div>
+          {/* 7_rating field */}
+          <div className="mb-8 form-control w-full">
+            <label className="label">
+              <span className="label-text text-lg font-bold">Ratings</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="number"
+                name="ratings"
+                placeholder="product rating"
+                className="input input-bordered rounded-xl drop-shadow-lg w-full"
+              />
+            </label>
+          </div>
+          {/* SUBMIT BUTTON */}
+          <div className="form-control w-full">
+            <input
+              type="submit"
+              value="Add Product"
+              className="px-8 py-3 bg-red-50 text-red-800 border border-red-900 hover:bg-red-100 rounded-xl drop-shadow-2xl font-bold text-xl"
+            />
+          </div>
         </div>
       </form>
     </div>
--- src/pages/Home/Brands/CarBrands.jsx
+++ src/pages/Home/Brands/CarBrands.jsx
@@ -7,7 +7,7 @@ const CarBrands = ({ carBrandsData }) => {
       <h3 className="text-4xl font-black text-red-950">
         Available Global Car Brands
       </h3>
-      <div className="py-9 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+      <div className="py-9 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
         {carBrandsData.map((carBrandData) => (
           <CarBrandCard
             key={carBrandData.id}
