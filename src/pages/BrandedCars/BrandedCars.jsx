7ab7866d4413fc9a7c89c0de9cabaf5df59280fd
Alien Brain <murad.dub@gmail.com>
2023-10-20 16:47:14 +0600

    message for no data to user handled in a specific brand car

--- src/pages/BrandedCars/BrandedCars.jsx
+++ src/pages/BrandedCars/BrandedCars.jsx
@@ -13,23 +13,36 @@ const BrandedCars = () => {
   // console.log(filteredBrandedCars);
   return (
     <>
-      {" "}
       <div>
         <BrandAdBanner name={name}></BrandAdBanner>
       </div>
-      <div className="text-center my-10  container mx-auto">
-        <h3 className="text-4xl font-black text-green-950">
-          OUR {name} BRAND CARS
-        </h3>
-        <div className="py-9 grid grid-cols-1 md:grid-cols-2 gap-10">
-          {filteredBrandedCars.map((brandedCar) => (
-            <BrandedCarCard
-              key={brandedCar._id}
-              brandedCar={brandedCar}
-            ></BrandedCarCard>
-          ))}
+      {filteredBrandedCars.length === 0 ? (
+        <div className="container mx-auto flex justify-center items-center min-h-screen bg-green-800 text-white font-bold text-3xl my-10">
+          <div>
+            <p>
+              Sorry! No Data Available For Brand:{" "}
+              <span className="text-red-800">{name} </span>!
+            </p>
+            <p>Please Visit Later.</p>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div>
+          <div className="text-center my-10  container mx-auto">
+            <h3 className="text-4xl font-black text-green-950">
+              OUR {name} BRAND CARS
+            </h3>
+            <div className="py-9 grid grid-cols-1 md:grid-cols-2 gap-10">
+              {filteredBrandedCars.map((brandedCar) => (
+                <BrandedCarCard
+                  key={brandedCar._id}
+                  brandedCar={brandedCar}
+                ></BrandedCarCard>
+              ))}
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 };
