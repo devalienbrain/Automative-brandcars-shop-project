a2de5203d3b6e0df5d7dd392910056009bd69e1b
Alien Brain <murad.dub@gmail.com>
2023-10-21 20:25:27 +0600

    project (client side) finalized

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -22,15 +22,15 @@ const Banner = ({ setIsDarkMode, isDarkMode }) => {
       <div className="hero-overlay bg-opacity-10"></div>
       <div className="hero-content text-center">
         <div>
-          <h1 className="mb-5 text-2xl md:text-5xl text-white font-black">
+          <h1 className="mb-5 text-2xl md:text-3xl lg:text-4xl text-white font-black">
             CHOOSE FAVOURITE ONE AND BUY YOUR NEXT CLASSIC CAR
           </h1>
-          <h3 className="mb-5 text-green-950 font-black">
+          <h3 className="mb-5 text-green-800 font-black">
             roadstar - the best car dealer in town
           </h3>
           <div className="py-10">
             <button
-              className="border border-white rounded-lg text-white px-4 py-1 font-semibold hover:text-black hover:border-none"
+              className="border border-white rounded-2xl text-sm text-white px-4 py-1 font-semibold hover:bg-white hover:text-black hover:border-none"
               onClick={handleThemeToggle}
             >
               Go {isDarkMode ? "Light" : "Dark"}
